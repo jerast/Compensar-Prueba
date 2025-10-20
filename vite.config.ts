f72a531e978269645bee56d9b7e0d89fc282d23f
Jerast <jose.romero.9602@outlook.com>
2025-10-20 12:34:20 -0500

    chore: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/shared', import.meta.url))
     }
   }
 });
