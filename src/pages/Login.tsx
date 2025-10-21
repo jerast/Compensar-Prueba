5596327c2b08b3a56e9cf54b7c736fe09365f9bd
Jerast <jose.romero.9602@outlook.com>
2025-10-20 20:00:29 -0500

    style: add login responsive desing

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,45 +10,45 @@ const Login = () => {
   return (
     <>
       <Header />
-      <main className="login">
-        <div className="login-info">
+      <main className="auth">
+        <div className="auth-info">
           <div>
-            <h1 className="login-info__title">Bienvenido</h1>
-            <span className="login-info__subtitle mb-14">Ingresa y disfruta</span>
-            <span className="login-info__description">
+            <h1 className="auth-info__title">Bienvenido</h1>
+            <span className="auth-info__subtitle">Ingresa y disfruta</span>
+            <span className="auth-info__description">
               Si aún no tienes una cuenta
               <br />
               puedes{' '}
             </span>
-            <Link className="login-info__link" to="/signup">
+            <Link className="auth-info__link" to="/signup">
               Registrarte aquí!
             </Link>
           </div>
-          <img className="login-info__img" src={LoginImage} aria-hidden />
+          <img className="auth-info__img" src={LoginImage} aria-hidden />
         </div>
 
-        <form className="login-form">
-          <h2 className="login-form__title">Iniciar sesión</h2>
-          <label className="login-form__input">
+        <form className="auth-form">
+          <h2 className="auth-form__title">Iniciar sesión</h2>
+          <label className="auth-form__input my-5">
             <input type="text" placeholder="Email o nombre de usuario" />
           </label>
-          <label className="login-form__input">
+          <label className="auth-form__input my-5">
             <input type="password" placeholder="Contraseña" />
             <button type="button">
               <EyeIcon />
             </button>
           </label>
-          <button className="login-form__action" type="button">
+          <button className="auth-form__action" type="button">
             Olvidé mi contraseña
           </button>
-          <button className="login-form__submit" type="submit">
+          <button className="auth-form__submit" type="submit">
             Iniciar sesión
           </button>
-          <Link className="login-form__link" to="/signup">
+          <Link className="auth-form__link" to="/signup">
             Registrarte aquí!
           </Link>
-          <span className="login-form__span">o continúa con</span>
-          <div className="login-form__socials">
+          <span className="auth-form__span">o continúa con</span>
+          <div className="auth-form__socials">
             <button type="button">
               <FBIcon />
             </button>
